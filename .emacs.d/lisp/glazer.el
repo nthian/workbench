5863fd2400261cf2345d853fa6b171b3a16a985a
Ian Thiele <icthiele@gmail.com>
2024-04-17 22:43:53 +0200

    Updated colors and decided I don't like highlight line mode.

--- .emacs.d/lisp/glazer.el
+++ .emacs.d/lisp/glazer.el
@@ -1,6 +1,3 @@
-(require 'hl-line)
-(global-hl-line-mode)
-
 (defun glazer/disable-custom-themes ()
   (mapcar (lambda (x) (disable-theme x)) custom-enabled-themes))
 
@@ -13,7 +10,9 @@
   (set-face-attribute 'highlight nil
 		      :background "#F5DEb3")
   (set-face-attribute 'mode-line nil
-		      :box nil)
+		      :box nil
+		      :background "grey75"
+		      :foreground "black")
   (setq frame-background-mode 'light))
 
 (defun glazer/dark-theme ()
@@ -23,20 +22,29 @@
   (set-face-attribute 'region nil
 		      :background "#6A5ACD")
   (set-face-attribute 'highlight nil
-		      :background "#483D8B"
+		      :background "#473c8b"
 		      :distant-foreground "gtk_selection_fg_color")
+  (set-face-attribute 'mode-line nil
+		      :box nil
+		      :foreground "#D0CFCC"
+		      :background "#473c8b")
   (setq frame-background-mode 'dark))
 
 (defvar glazer/theme-list
   '(glazer/light-theme glazer/dark-theme))
+
+(defun glazer/apply-theme ()
+  (apply (seq-first glazer/theme-list) nil))
 
 (defun glazer/toggle-theme ()
   (interactive)
   (glazer/disable-custom-themes)
   (setq glazer/theme-list
 	(append (cdr glazer/theme-list) (list (car glazer/theme-list))))
-  (apply (seq-first glazer/theme-list) nil)
+  (glazer/apply-theme)
   (mapcar #'frame-set-background-mode (visible-frame-list)))
+
+(add-hook 'server-after-make-frame-hook #'glazer/apply-theme)
 
 ;;; Look and feel
 (tool-bar-mode -1)
